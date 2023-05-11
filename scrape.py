9849f9beab8bdc2ef99cdca18de99ce569bd8d78
StevenCloud <stevencloud9@gmail.com>
2023-05-11 16:58:13 -0500

    scrapes all text on site

---
+++ scrape.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_site_text(url):
+    '''
+    Get the text from a website
+
+    Parameters
+    ----------  
+    url : str
+        The url of the website to scrape
+
+    Returns
+    -------
+    str
+        The text from the website
+    '''
+    # Get the website
+    response = requests.get(url)
+
+    # Raise exception for unsuccessful status codes
+    response.raise_for_status()
+
+    # Parse the website
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Get the text
+    text = soup.get_text()
+
+    return text
+
+print(get_site_text('https://www.python.org/'))
